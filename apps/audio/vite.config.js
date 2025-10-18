49aacac342950d1fe6c6b09548669639c4bfe9cc
ThomasTsao0704 <120099666+ThomasTsao0704@users.noreply.github.com>
2025-10-18 23:08:41 +0800

    Update vite.config.js

--- apps/audio/vite.config.js
+++ apps/audio/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: '.', // 專案根目錄是當前資料夾
   build: {
-    outDir: 'site',
+    outDir: 'site', // 對應 workflow 的輸出資料夾
     rollupOptions: {
       input: 'index.html'
     }
